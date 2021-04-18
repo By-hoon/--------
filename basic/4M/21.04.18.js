8c2afe00166a4efb5afdef6356b45edc46aa0fc9
연훈 배 <sbaru1997@naver.com>
2021-04-18 17:05:00 +0900

    21.04.18 -1

---
+++ basic/4M/21.04.18.js
@@ -0,0 +1,18 @@
+const yhInfo = {
+    name:"yeunhun",
+    age:25,
+    gender:"Male",
+    isHandsome:true,
+    favColor:["Skyblue", "Bluegreen"],
+    favFood: [
+        {
+            name: "Sushi",
+            fatty: true
+        },
+        {
+            name: "Salad",
+            fatty: false
+        }
+    ]
+}
+console.log(yhInfo)
