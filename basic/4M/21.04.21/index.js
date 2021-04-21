a5e9f58ac77062d9aca718b58bcd8ac3fa47e0cc
연훈 배 <sbaru1997@naver.com>
2021-04-21 17:38:24 +0900

    21.04.21 -2

--- basic/4M/21.04.21/index.js
+++ basic/4M/21.04.21/index.js
@@ -1,5 +1,30 @@
-const title = document.getElementById("title");
-title.innerHTML = "Hi From JS";
-title.style.color = "red";
-document.tilte = "I own you now";
+const title = document.querySelector("h2");
 
+const superEventHandler = {
+  function: [
+    function handleenter() {
+      title.innerHTML = "The mouse is here";
+      title.style.color = "#1abc9c";
+    },
+
+    function handleleave() {
+      title.innerHTML = "The mouse is gone";
+      title.style.color = "#3498db";
+    },
+
+    function handleResize() {
+      title.innerHTML = "You just resized!";
+      title.style.color = "#9b59b6";
+    },
+
+    function handleAuxclick() {
+      title.innerHTML = "That was a right click!";
+      title.style.color = "#e74c3c";
+    }
+  ]
+};
+
+title.addEventListener("mouseenter", superEventHandler.function[0]);
+title.addEventListener("mouseleave", superEventHandler.function[1]);
+window.addEventListener("resize", superEventHandler.function[2]);
+window.addEventListener("auxclick", superEventHandler.function[3]);
