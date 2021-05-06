114637787bee6890114fa0aaf3042bc4974362de
연훈 배 <sbaru1997@naver.com>
2021-05-06 21:27:57 +0900

    21.05.06 -1

--- basic/hoonap(kakao)/map.js
+++ basic/hoonap(kakao)/map.js
@@ -191,8 +191,32 @@ function displayArea(area) {
         polygon.setOptions({fillColor: '#fff'});
         customOverlay.setMap(null);
     }); 
+}
 
-    // 다각형에 click 이벤트를 등록하고 이벤트가 발생하면 다각형의 이름과 면적을 인포윈도우에 표시합니다 
-    kakao.maps.event.addListener(polygon, 'click', function(mouseEvent) {
-    });
+function addMarker(){
+var cnt = 0;
+function makeMarker(mouseEvent){
+    if(cnt === 0){
+    var latlng = mouseEvent.latLng; 
+    var imageSrc = 'KakaoTalk_20200623_145500408.JPG', // 마커이미지의 주소입니다    
+    imageSize = new kakao.maps.Size(20, 20), // 마커이미지의 크기입니다
+    imageOption = {offset: new kakao.maps.Point(10 , 20)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+// 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+    markerPosition = new kakao.maps.LatLng(latlng.getLat(), latlng.getLng()); // 마커가 표시될 위치입니다
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+  position: markerPosition,
+  image: markerImage // 마커이미지 설정 
+
+});
+}
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);  
+cnt = 1;
+}
+kakao.maps.event.addListener(map, 'click', makeMarker);
+
 }
