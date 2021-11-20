7feacf3102340911313ad07ee7780191ad082459
연훈 배 <sbaru1997@naver.com>
2021-11-20 23:06:18 +0900

    feat: react-beautiful-dnd로 드래그 드랍 기능 구현

--- Trello/src/App.tsx
+++ Trello/src/App.tsx
@@ -1,7 +1,26 @@
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
 function App() {
+  const onDragEnd = () => { };
   return (
-    <>
-    </>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId="one">
+          {(magic) => (
+            <ul ref={magic.innerRef} {...magic.droppableProps}>
+              <Draggable draggableId="first" index={0}>
+                {(magic) => (
+                  <li ref={magic.innerRef} {...magic.draggableProps}>
+                    <span {...magic.dragHandleProps}>🔥</span>
+                    One
+                  </li>
+                )}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
   );
 }
 
